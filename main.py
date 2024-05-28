70e91364f8750be6bff4556e32dd844b2d58a1f1
bmfischer3 <30556905+bmfischer3@users.noreply.github.com>
2024-05-28 10:10:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+# Golf
+# Goal 1 - Keep track of score for multiple players during a round of 18
+# Milesstones - Keep track of score for one player, and ask for the course name and location. 
+# Later on, keep this in a database. 
+
+
+class Player:
+    def __init__(self, player_name, handicap):
+        self.player_name = player_name
+        self.handicap = handicap
+
+class Round:
+    def __init__(self, course_name, course_location, slope, yardage, hole_list):
+        self.course_name = course_name
+        self.course_location = course_location
+        self.slope = slope
+        self.yardage = yardage
+        self.players = []
+        self.hole_list = hole_list
+        self.scorecard = {}
+
+    def add_players(self, player_list):
+        self.player_list = player_list
+        for i in player_list:
+            self.players.append(i)
+            print(f"{i} has been successfully added to the round.")
+        # Create scorekeeping dictionary
+        self.scorecard.update({player: self.hole_list.copy() for player in player_list})
+    
+    def remove_player(self, player_name):
+        if player_name in self.players:
+            self.players.remove(player_name)
+            del self.scorecard[player_name]
+            print(f"{player_name} has been successfully removed from this round.")
+            print(self.scorecard)
+        else:
+            print(f"{player_name} is not in this round.")
+    
+    def modify_score(self, player_name, hole_number, stroke_count):
+        player_score = self.scorecard.get(player_name)
+        if player_score is not None:
+            player_score[hole_number] = stroke_count
+        print(player_score)
+
+pebble_hole_list = {
+    1:0,
+    2:0,
+    3:0,
+    4:0
+}
+
+brian = Player("Brian", 15)
+pebble = Round("Pebble Beach", "Del Monte, CA", 145.0, 6828, pebble_hole_list)
+
+players = ["Brian", "Kyle", "Jack", "Robin"]
+
+print(pebble.add_players(players))
+
+pebble.modify_score("Brian", 2, 4)
+pebble.modify_score("Robin", 2, 3)
+
+print(pebble.scorecard)
+
+
