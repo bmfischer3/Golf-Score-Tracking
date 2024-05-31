852f8311317257f251e95e3ba78f020979381a78
bmfischer3 <30556905+bmfischer3@users.noreply.github.com>
2024-05-30 23:30:00 -0400

    added totaling functions

--- main.py
+++ main.py
@@ -8,7 +8,8 @@
 
 
 
-
+# import psycopg2
+# from config import load_config
 import time
 
 class Player:
@@ -17,6 +18,28 @@ class Player:
         self.handicap = handicap
 
 class Round:
+    
+    STARTING_SCORES = {
+            1:0,
+            2:0,
+            3:0,
+            4:0,
+            5:0,
+            6:0,
+            7:0,
+            8:0,
+            9:0,
+            10:0,
+            11:0,
+            12:0,
+            13:0,
+            14:0,
+            15:0,
+            16:0,
+            17:0,
+            18:0
+        }
+    
     def __init__(self, course_name, course_location, slope, yardage, hole_list):
         self.course_name = course_name
         self.course_location = course_location
@@ -26,13 +49,41 @@ class Round:
         self.hole_list = hole_list
         self.scorecard = {}
 
+        self._title = ""
+ 
+    
+    def get_all_totals(self) -> dict:
+        """Get totals of all players in 
+
+
+
+        Returns:
+            dict: ex
+            {
+                "brye": 69
+            } 
+        """
+        return NotImplementedError
+    
+    @property
+    def title(self) -> None:
+        """set current total
+        """
+        return self._title
+    
+    @title.setter
+    def set_title(self, name: str):
+        self._title = name.strip("The ")
+
+
+
     def add_players(self, player_list):
         self.player_list = player_list
         for i in player_list:
             self.players.append(i)
             print(f"{i} has been successfully added to the round.")
         # Create scorekeeping dictionary
-        self.scorecard.update({player: self.hole_list.copy() for player in player_list})
+        self.scorecard.update({player: STARTING_SCORES.copy() for player in player_list})
     
     def remove_player(self, player_name):
         if player_name in self.players:
@@ -47,7 +98,6 @@ class Round:
         player_score = self.scorecard.get(player_name)
         if player_score is not None:
             player_score[hole_number] = stroke_count
-        print(player_score)
 
 
     def get_nth_hole(self, n=0):
@@ -61,7 +111,7 @@ class Round:
     def complete_hole(self):
         print("The round is starting...")
         time.sleep(2)
-        starting_hole_num = int(input(f"What hole number are you starting on? "))
+        starting_hole_num = int(input(f"What hole number are you updating?"))
         for name, scorecard in self.scorecard.items():
             for hole_num, score in scorecard.items():
                 hole_key = (list(scorecard)[0] + starting_hole_num - 1)
@@ -70,15 +120,54 @@ class Round:
                     self.modify_score(name, hole_num, strokes)
                     print(f"{name}'s score for hole #{hole_num} has been updated to {strokes}.")
 
+    def submit_round(self):
+        for name, scorecard in self.scorecard.items():
+            scorecard_values = scorecard.values()
+            if 0 in scorecard_values:
+                print(f"There are still 0s in {name}'s the scorecard, please review.")
+                # start function to fix scorecards. Will insert this later. 
+            else:
+                print(f"{name}'s scorecard is complete and you are ready to submit.")
+                # start uplading scorecard here. 
+                print(f"{name}'s scorecard is being uploaded...")
+                # course_name = self.course_name
+
+                # sql = """INSERT INTO golf_scores """
+                # conn = psycopg2.connect(dsn)
+                # cur = conn.cursor()
+
+
+
+
+                
+        # Check that all holes have been completed by all parties, if not, go back through and add those scores to the state. 
+        # Determine what the date is for this round. 
+        # Submit the scores to the database as if they were all logged individually. 
+        # Data will need to be manipulated nad parsed so it will correclty enter the database. 
+
+
 
 
 pebble_hole_list = {
-    1:0,
-    2:0,
-    3:0,
-    4:0,
-    5:0
-}
+    1:4,
+    2:5,
+    3:4,
+    4:4,
+    5:3,
+    6:5,
+    7:3,
+    8:4,
+    9:4,
+    10:4,
+    11:4,
+    12:3,
+    13:4,
+    14:5,
+    15:4,
+    16:4,
+    17:3,
+    18:5
+    }
 
 brian = Player("Brian", 15)
 pebble = Round("Pebble Beach", "Del Monte, CA", 145.0, 6828, pebble_hole_list)
@@ -87,10 +176,12 @@ players = ["Brian", "Kyle", "Jack", "Robin"]
 
 print(pebble.add_players(players))
 
-# pebble.modify_score("Brian", 2, 4)
+pebble.modify_score("Brian", 2, 5)
 # pebble.modify_score("Robin", 2, 3)
 
 print(pebble.scorecard)
-pebble.complete_hole()
 
-# 
+
+pebble.total = 5
+
+pebble.get_all_totals()
